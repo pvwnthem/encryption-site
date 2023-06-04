c0e0086dce0b36ab8f3125760e55610a3961c7ac
pvwnthem <pvwn.them@gmail.com>
2023-06-04 16:05:33 -0400

    add download.js, fix downloading

--- src/components/DecryptForm.tsx
+++ src/components/DecryptForm.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import cryptoService from '@/services/encryption.service';
 import TextHelper from '@/util/base64';
+import download from 'downloadjs';
 
 const DecryptForm = () => {
   const [encryptedData, setEncryptedData] = useState<any>(null);
@@ -13,7 +14,36 @@ const DecryptForm = () => {
     setEncryptedData(event.target.value);
   };
   
+  function extractMimeTypeFromBase64(base64Data: string | null): string {
+    const regex = /^data:(.+);base64,/; // Regular expression to match the MIME type
+    const matches = base64Data?.match(regex);
+    if (matches && matches.length > 1) {
+      return matches[1]; // Extract the MIME type from the matched groups
+    } else {
+      return ''; // MIME type not found
+    }
+  }
 
+  function generateFileExtension(mimeType: string) {
+    const mimeToExtMap: any = {
+      'image/jpeg': '.jpg',
+      'image/png': '.png',
+      'image/gif': '.gif',
+      'image/svg+xml': '.svg',
+      'audio/mpeg': '.mp3',
+      'audio/wav': '.wav',
+      'video/mp4': '.mp4',
+      'video/webm': '.webm',
+      // Add more MIME types and their corresponding file extensions as needed
+    };
+  
+    if (mimeType in mimeToExtMap) {
+      return mimeToExtMap[mimeType];
+    } else {
+      return null; // File extension not found
+    }
+  }
+   
   function onSelectFile(e: React.ChangeEvent<HTMLInputElement>) {
     if (e.target.files && e.target.files.length > 0) {
       const reader = new FileReader();
@@ -95,7 +125,8 @@ const DecryptForm = () => {
           <pre>{decryptedData}</pre>
           <img src={decryptedData}></img>
           {fileUrl && (
-            <a href={fileUrl} download="encrypted_file.bin">
+            
+            <a onClick={() => download(decryptedData, "decrypted" + generateFileExtension(extractMimeTypeFromBase64(decryptedData)), extractMimeTypeFromBase64(decryptedData))}>
               Download Decrypted File
             </a>
           )}
