37dda5d5c41ece97e51d6c91c2f2c50a9bf953d3
pvwnthem <pvwn.them@gmail.com>
2023-06-03 20:26:29 -0400

    more testing of encryption system

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import TextHelper from '@/util/base64';
 
 export default function Home() {
   const [encryption, setEncryption] = React.useState<any>(null);
+  const [decryption, setDecryption] = React.useState<any>(null);
   const [text, setText] = React.useState<string | null>(null)
   React.useEffect(() => {
     if (text) {
@@ -22,10 +23,31 @@ export default function Home() {
       }
     }
   }, [text]);
+
+  React.useEffect(() => {
+    if (encryption) {
+      const keyBuffer = TextHelper.convertBase64ToStream(encryption.key);
+
+      encryptionService.parseKey(keyBuffer).then((key) => {
+        const cipherBuffer = TextHelper.convertBase64ToStream(
+          encryption.cipher
+        );
+
+        const ivBuffer = TextHelper.convertBase64ToStream<Uint8Array>(
+          encryption.iv
+        );
+
+        encryptionService.decrypt(cipherBuffer, key, ivBuffer).then((res) =>
+          setDecryption(res)
+        );
+      });
+    }
+  }, [encryption]);
   
   return (
     <>
     {JSON.stringify(encryption)}
+    {JSON.stringify(decryption)}
     </>
   )
 }
